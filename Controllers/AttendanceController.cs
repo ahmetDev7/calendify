5bcdad2dbdb85e48b94400d5423d3021a90866e5
sidi <sidimo2001@gmail.com>
2024-09-30 15:48:36 +0200

    feat: get and delete attendancee

--- Controllers/AttendanceController.cs
+++ Controllers/AttendanceController.cs
@@ -1,19 +1,58 @@
+using System.Data.Common;
+using calendify.Data;
+using calendify_app.Models;
 using Microsoft.AspNetCore.Mvc;
+using static AttendanceService;
 
 namespace calendify.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 public class AttendanceController : ControllerBase
 {
-    public AttendanceController()
+    private readonly AttendanceService _attendanceService;
+    public AttendanceController(AppDbContext db)
     {
-        
+        _attendanceService = new AttendanceService(db);
     }
 
-    [HttpGet()]
-    public bool Get()
+    [HttpGet("all")]
+    public IActionResult GetAllAttendance()
     {
-        return false;
+        return Ok(_attendanceService.GetAllAttendance());
     }
+
+    [HttpGet("{id}")]
+    public ActionResult<AttendanceResult> GetAttendanceByID(Guid id)
+    {
+        var attendanceItem = _attendanceService.GetAttendanceById(id);
+        if (attendanceItem == null) 
+            {
+                return NotFound(new {message = "Attendancee not found."});
+            }
+            return Ok(attendanceItem);
+    }
+
+    [HttpGet("user-id/{userId}")]
+    public ActionResult<List<AttendanceResult>> GetAttendanceByUserId(Guid userId)
+    {
+        var attendanceItem = _attendanceService.GetAttendancesByUserId(userId);
+        if (attendanceItem == null) 
+            {
+                return NotFound(new {message = "Attendancee not found. Based on UserId"});
+            }
+            return Ok(attendanceItem);
+    }
+
+    [HttpDelete("{id}")]
+        public IActionResult DeleteAttendance(Guid id)
+        {
+            bool isDeleted = _attendanceService.DeleteAttendance(id);
+            if (!isDeleted)
+            {
+                return BadRequest("Attendance id not found!");
+            }
+            return Ok(new {message = "Attendance succesfully deleted."});
+        }
+
 }
---
+++ Services/AttendanceService.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using calendify.Data;
+using calendify_app.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class AttendanceService
+{
+    private readonly AppDbContext _db;
+    public AttendanceService(AppDbContext db)
+    {
+        _db = db;
+    }
+
+
+
+    public List<AttendanceResult> GetAllAttendance()
+    {
+        return _db.Attendance
+                  .Select(a => new AttendanceResult
+                  {
+                      Id = a.Id,
+                      Date = a.Date,
+                      UserId = a.UserId
+                  })
+                  .ToList();
+    }
+
+    // get single attendance by attendance_id
+    public AttendanceResult? GetAttendanceById(Guid id)
+    {
+        return _db.Attendance.Select(a => new AttendanceResult
+        {
+            Id = a.Id,
+            Date = a.Date,
+            UserId = a.UserId
+        }).FirstOrDefault(e => e.Id == id);
+    }
+
+    // get all attendances based on user id
+    // SELECT id, date, userId FROM attendance WHERE UserId = "uuid-388735"
+    // Get all attendances based on user id (returning full Attendance objects)
+    public List<AttendanceResult> GetAttendancesByUserId(Guid userId)
+    {
+        return _db.Attendance.Select(a => new AttendanceResult
+        {
+            Id = a.Id,
+            Date = a.Date,
+            UserId = a.UserId
+        }).Where(e => e.UserId == userId).ToList();
+    }
+
+
+
+    public bool DeleteAttendance(Guid attendanceId)
+    {
+        var attendanceToDelete = _db.Attendance.FirstOrDefault(e => e.Id == attendanceId);
+        if (attendanceToDelete == null)
+        {
+            return false;
+        }
+
+        _db.Attendance.Remove(attendanceToDelete);
+
+        _db.SaveChanges();
+
+        return true;
+    }
+
+    public class AttendanceResult
+    {
+        public Guid Id { get; set; }
+        public DateTime Date { get; set; }
+        public Guid UserId { get; set; }
+    }
+
+
+}
