9dd406e5a408cf67c00fcd40306255f87cbe61bf
Ahmet <38209407+ahmetDev7@users.noreply.github.com>
2024-09-30 12:54:13 +0200

    Removed EF authentication and added simple authentication

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,21 +1,19 @@
 using calendify_app.Models;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace calendify.Data
 {
-    public class AppDbContext : IdentityDbContext<IdentityUser>
+    public class AppDbContext: DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
-        }
 
+        }
 
         public DbSet<Admin> Admin { get; set; }
         public DbSet<Attendance> Attendance { get; set; }
         public DbSet<Event> Event { get; set; }
-        public DbSet<EventAttendance> Event_Attendance { get; set; }
+        public DbSet<EventAttendance> Event_Attendance { get; set; }        
         public DbSet<User> User { get; set; }
     }
 }
