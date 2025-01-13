a0235f5604dc0a15d237e64e4785490cd9808bc5
Ahmet <38209407+ahmetDev7@users.noreply.github.com>
2025-01-13 18:07:11 +0100

    feature: added router & register

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,25 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar.tsx'
+import Register from './components/Register.tsx';
+
 function App() {
   return (
     <>
-    <h1 className='text-6xl'>Calendify</h1>
+      <Router>
+        <Navbar/>
+        <div className="content">
+          <div className="content-inner">
+            <Routes>
+              <Route path="/" element={<div>Home Page</div>} />
+              <Route path="/about" element={<div>About Page</div>} />
+              <Route path="/services" element={<div>Services Page</div>} />
+              <Route path="/contact" element={<div>Contact Page</div>} />
+              <Route path="/login" element={<div>Login Page</div>} />
+              <Route path="/register" element={<Register/>} />
+            </Routes>
+          </div>
+        </div>
+      </Router>
     </>
   )
 }
---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <>
+      <nav className="bg-white border-gray-200 dark:bg-gray-900">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <NavLink to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+            <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Calendify Logo" />
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Calendify</span>
+          </NavLink>
+          <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+            <NavLink to="/login">
+              <button type="button" className="mx-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button>
+            </NavLink>
+            <NavLink to="/register">
+              <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register</button>
+            </NavLink>
+          </div>
+          <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
+            <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <li>
+                <NavLink
+                  to="/"
+                  className={({ isActive }: { isActive: boolean }) => 
+                    `block py-2 px-3 md:p-0 ${isActive ? 'text-blue-500' : 'text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'}`
+                  }
+                  aria-current="page"
+                >
+                  Dashboard
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/about"
+                  className={({ isActive }: { isActive: boolean }) => 
+                    `block py-2 px-3 md:p-0 ${isActive ? 'text-blue-500' : 'text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'}`
+                  }
+                >
+                  About
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/services"
+                  className={({ isActive }: { isActive: boolean }) => 
+                    `block py-2 px-3 md:p-0 ${isActive ? 'text-blue-500' : 'text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'}`
+                  }
+                >
+                  Services
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/contact"
+                  className={({ isActive }: { isActive: boolean }) => 
+                    `block py-2 px-3 md:p-0 ${isActive ? 'text-blue-500' : 'text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'}`
+                  }
+                >
+                  Contact
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
